205ccd36dd6599e1fe50330da9f302f046e60ab5
cyrus3690 <serve3690@gmail.com>
2023-02-18 22:51:14 +0900

    [update] 실습 2일차 종료

--- cpp/training2.cpp
+++ cpp/training2.cpp
@@ -100,35 +100,78 @@ int main()
 
   // cout << number << s;
 
-  // 입력문
-  cout << "Please input start day number : ";
-  int number;
-  cin >> number;
-  cout << "Please input end date : ";
-  int endDate;
-  cin >> endDate;
+  // 달력 만들기
+  //  입력문
+  // cout << "Please input start day number : ";
+  // int number;
+  // cin >> number;
+  // cout << "Please input end date : ";
+  // int endDate;
+  // cin >> endDate;
 
-  // 요일
-  char day[7] = {'M', 'T', 'W', 'T', 'F', 'S', 'S'};
-  for (int i = 0; i < 7; i++)
-  {
-    cout << setw(3) << day[i];
-  }
-  cout << "\n";
+  // // 요일
+  // char day[7] = {'M', 'T', 'W', 'T', 'F', 'S', 'S'};
+  // for (int i = 0; i < 7; i++)
+  // {
+  //   cout << setw(3) << day[i];
+  // }
+  // cout << "\n";
 
-  for (int date = 1 - number, newLine = 0; date <= 31; date++, newLine++) // 초기화를 두개 시킬 수 있다.
-  {
-    if (date < 1)
-    {
-      cout << setw(3) << " "; // 음수일 경우 공백 출력
-    }
-    else // 양수일 경우 1~31까지 출력
-    {
-      cout << setw(3) << date;
-      if (newLine % 7 == 6) // 6번째에서 개행시킨다.
-      {
-        cout << endl;
-      }
-    }
-  }
+  // for (int date = 1 - number, newLine = 0; date <= 31; date++, newLine++) // 초기화를 두개 시킬 수 있다.
+  // {
+  //   if (date < 1)
+  //   {
+  //     cout << setw(3) << " "; // 음수일 경우 공백 출력
+  //   }
+  //   else // 양수일 경우 1~31까지 출력
+  //   {
+  //     cout << setw(3) << date;
+  //     if (newLine % 7 == 6) // 6번째에서 개행시킨다.
+  //     {
+  //       cout << endl;
+  //     }
+  //   }
+  // }
+  // int month;
+  // cout << "월을 입력하시오. : ";
+  // cin >> month;
+  // int days[12]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+  // cout << month << "월은 " << days[month - 1] << "일까지 있습니다." << endl;
+
+  // 범위 함수를 이용한 아스키코드 대문자 변환
+  // cout << "Please input a string :";
+  // string str;
+  // getline(cin, str);
+  // for (int i : str)
+  // {
+  //   if (i >= 97 && i <= 122)
+  //   {
+  //     i -= 32;
+  //   }
+  //   cout << char(i);
+  // }
+
+  // 인간승리다 ㄹㅇ
+  // string str;
+  // cout << "문자열을 입력하시오 : ";
+  // getline(cin, str);
+  // int alpha[26] = {};
+  // for (char ch : str)
+  // {
+  //   if (ch >= 'A' && ch <= 'z')
+  //   {
+  //     int n = ch - 'a';
+  //     if (n >= 0)
+  //       alpha[n]++;
+  //     else
+  //       alpha[n + 'a' - 'A']++;
+  //   }
+  // }
+  // int n = 0;
+  // while (n < 26)
+  // {
+  //   cout << char('a' + n) << " : " << alpha[n] << endl;
+  //   n++;
+  // }
 }
